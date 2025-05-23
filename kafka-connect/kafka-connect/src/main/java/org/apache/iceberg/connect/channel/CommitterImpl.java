ced58e3d64b095e2b833525fdecfd2fae48780b2
Bertrand Paquet <bertrand.paquet@gmail.com>
2025-05-23 18:14:17 +0200

    WIP

--- kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/channel/CommitterImpl.java
+++ kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/channel/CommitterImpl.java
@@ -61,7 +61,7 @@ public class CommitterImpl implements Committer {
   @Override
   public void start(Catalog catalog, IcebergSinkConfig config, SinkTaskContext context) {
     Map<String, String> props = new HashMap<>(config.kafkaProps());
-    props.put("max.poll.records", "2");
+    props.put("consumer.max.poll.records", "2");
     LOG.info("Kafka properties for committer: {}", props);
     KafkaClientFactory clientFactory = new KafkaClientFactory(props);
 
